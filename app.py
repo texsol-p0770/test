a85eae2f03dad344c68dbf1971c29cb10739353b
texsol-p0770 <p0770@pt.wni.co.jp>
2018-12-26 17:49:27 +0900

    test

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def message_received(client, server, message):
 	print(json.dumps(hash))
 
 if __name__ == "__main__":
-	server = WebsocketServer(1064, host="wsstest-sky-sandbox.pt-wrap-ea.wni.com")
+	server = WebsocketServer(1064, host="127.0.0.1")
 	server.set_fn_new_client(new_client)
 	server.set_fn_client_left(client_left)
 	server.set_fn_message_received(message_received)
