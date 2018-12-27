33ea5d6fdde1bbf80bb2f1baa43bd56a365414b5
texsol-p0770 <p0770@pt.wni.co.jp>
2018-12-27 14:13:07 +0900

    test

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from websocket_server import WebsocketServer
 from datetime import datetime
 import json
-import logging
 import time
 import traceback
 
@@ -26,7 +25,7 @@ def message_received(client, server, message):
 
 if __name__ == "__main__":
 	try:
-		server = WebsocketServer(8080, host="0.0.0.0", loglevel=logging.INFO)
+		server = WebsocketServer(8080, host="0.0.0.0")
 		server.set_fn_new_client(new_client)
 		server.set_fn_client_left(client_left)
 		server.set_fn_message_received(message_received)
