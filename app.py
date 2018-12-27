d39259af58636b86f1eee2b62e37a64e7cb9ddca
texsol-p0770 <p0770@pt.wni.co.jp>
2018-12-27 12:44:08 +0900

    test

--- app.py
+++ app.py
@@ -24,11 +24,11 @@ def message_received(client, server, message):
 
 if __name__ == "__main__":
 	try:
-	server = WebsocketServer(8080, host="0.0.0.0")
-	server.set_fn_new_client(new_client)
-	server.set_fn_client_left(client_left)
-	server.set_fn_message_received(message_received)
-	server.run_forever()
-except:
-	import traceback
-	traceback.print_exc()
+		server = WebsocketServer(8080, host="0.0.0.0")
+		server.set_fn_new_client(new_client)
+		server.set_fn_client_left(client_left)
+		server.set_fn_message_received(message_received)
+		server.run_forever()
+	except:
+		import traceback
+		traceback.print_exc()
