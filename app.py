1772a7f19e1a898fcfe53abef089ba1e87cde4b0
texsol-p0770 <p0770@pt.wni.co.jp>
2018-12-28 16:20:46 +0900

    test

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def message_received(client, server, message):
 
 if __name__ == "__main__":
 	try:
-		server = WebsocketServer(8080, host="0.0.0.0")
+		server = WebsocketServer(8000, host="0.0.0.0")
 		server.set_fn_new_client(new_client)
 		server.set_fn_client_left(client_left)
 		server.set_fn_message_received(message_received)
